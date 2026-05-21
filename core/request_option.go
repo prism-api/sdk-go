698782edb4466c4276c64edcb002c5f1ff86cb94
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-05-21 11:22:48 +0000

    Release v1.3.1

--- core/request_option.go
+++ core/request_option.go
@@ -58,8 +58,8 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/prism-api/sdk-go")
-	headers.Set("X-Fern-SDK-Version", "v1.3.0")
-	headers.Set("User-Agent", "github.com/prism-api/sdk-go/1.3.0")
+	headers.Set("X-Fern-SDK-Version", "v1.3.1")
+	headers.Set("User-Agent", "github.com/prism-api/sdk-go/1.3.1")
 	return headers
 }
 
